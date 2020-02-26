5b02a35f17fb6f34c320e320c3f1c06b4b1c2c5b
tgwarr <k.willard@hotmail.com>
2020-02-26 11:58:56 -0800

    Boundaries

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float speed = 10.0f;
+    const float horizontalBoundary = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +15,13 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x > 10)
+        if(transform.position.x > horizontalBoundary)
         {
-            transform.position = new Vector3(10,transform.position.y,transform.position.z);
+            transform.position = new Vector3(horizontalBoundary,transform.position.y,transform.position.z);
         }
-        if(transform.position.x < -10)
+        if(transform.position.x < -horizontalBoundary)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-horizontalBoundary, transform.position.y, transform.position.z);
         }
         float horizontal = Input.GetAxis("Horizontal");
 
