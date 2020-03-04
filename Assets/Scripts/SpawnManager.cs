75e27455c9baeb204d01883ce3c142d9ada74a68
CBTE Account <cbte@ac.sdmesa.net>
2020-03-04 11:27:57 -0800

    Spawn Point Randomizing

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private const float spawnPointZ = 25;
+    private const float xRange = 10;
+    public GameObject[] Animals;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            int randomSpawn = Random.Range(0,Animals.Length);
+            Instantiate(Animals[randomSpawn], new Vector3(Random.Range(-xRange,xRange),0,spawnPointZ), Animals[randomSpawn].transform.rotation);
+        }
+    }
+}
